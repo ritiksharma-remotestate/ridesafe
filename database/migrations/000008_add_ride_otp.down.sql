d643c5f38ca7e85c404c3c7af90ce9060513e8ac
Ritik Sharma <ritik.sharma@remotestate.in>
2026-07-27 14:51:59 +0530

    fixed db.go and other logics

--- database/migrations/000008_add_ride_otp.down.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE rides
-DROP COLUMN ride_otp_hash,
-
-DROP COLUMN ride_otp_generated_at,
-
-DROP COLUMN ride_otp_verified,
-
-DROP COLUMN ride_otp_attempts;
--- database/migrations/000009_add_otp_verified_at.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE rides
-DROP COLUMN otp_verified_at;
