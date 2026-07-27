21654b4c6d69abcac4b05f0d2b529b26448090c8
Ritik Sharma <ritik.sharma@remotestate.in>
2026-07-27 15:31:23 +0530

    updated user.go

--- handlers/user.go
+++ handlers/user.go
@@ -1,4 +1,79 @@
 package handlers
-/*func GetProfile(w http.ResponseWriter, r *http.Request)
-func UpdateProfile(w http.ResponseWriter, r *http.Request)
-func DeleteUser(w http.ResponseWriter, r *http.Request)*/
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"ridesafe/error_custom"
+	"ridesafe/models"
+
+	// "ridesafe/repository"
+	"ridesafe/services"
+	"ridesafe/utils"
+)
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	var user models.RegisterRequest
+
+	err := json.NewDecoder(r.Body).Decode(&user) // utils.paeseBody()
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	if errs := utils.CheckValidation(user); errs != nil {
+		utils.RespondError(w, http.StatusBadRequest, nil, "invalid request", errs.Error())
+
+		return
+	}
+
+	_, err = services.RegisterUser(user)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, error_custom.ErrUserAlreadyExists):
+			utils.RespondError(w, http.StatusConflict, err, "user already exists")
+
+		default:
+			utils.RespondError(w, http.StatusInternalServerError, err, "failed to create user")
+		}
+		return
+	}
+	utils.RespondJSON(w, http.StatusCreated, map[string]string{
+		"message": "user created successfully",
+	})
+
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var req models.LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	if errs := utils.CheckValidation(req); errs != nil {
+		utils.RespondError(w, http.StatusBadRequest, nil, "invalid request", errs.Error())
+		return
+	}
+	//  iknow this users struct is unnecessary but i already wrote code using it so i am not removing for now
+
+	token, err := services.Login(req.Email, req.Password)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, error_custom.ErrInvalidCredentials):
+			utils.RespondError(w, http.StatusUnauthorized, err, "invalid email or password")
+		default:
+			utils.RespondError(w, http.StatusInternalServerError, err, "failed to login")
+		}
+		return
+	}
+	utils.RespondJSON(w, http.StatusOK, struct {
+		Token string `json:"token"`
+	}{
+		Token: token,
+	})
+
+}
